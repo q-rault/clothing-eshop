85e505c7abedc04b2923f6234d3b2652543c4689
Quentin Rault <q.rault054@gmail.com>
2022-02-14 11:44:36 +0100

    added state to signin component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,11 +2,53 @@ import React from "react";
 
 import "./sign-in.styles.scss";
 
-const SignIn = ({}) => (
-  <div 
-  className="sign-in"
-  >
-  </div>
-);
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (e) => {
+    const { name } = e.target;
+    this.setState({ [name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2> I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Password</label>
+          <input type="submit" value="Submit Form"></input>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SignIn;
