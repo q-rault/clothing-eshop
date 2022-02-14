1e377dc4321171398094529db6b80d1408b467b9
Quentin Rault <q.rault054@gmail.com>
2022-02-14 16:08:21 +0100

    added reusable custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
 
 import "./sign-in.styles.scss";
@@ -26,7 +27,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className="sign-in">
-        <h2> I already have an account</h2>
+        <h2 className="title"> I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
           <FormInput
@@ -45,7 +46,7 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
-          <input type="submit" value="Submit Form"></input>
+          <CustomButton type="submit"> Sign in</CustomButton>
         </form>
       </div>
     );
