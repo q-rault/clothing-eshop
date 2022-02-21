8d6bda741c8772068622275cf80a10643134616a
Quentin Rault <q.rault054@gmail.com>
2022-02-21 12:20:32 +0100

    Adding Selectors for CartIcon, CartDropdown in order to save some rendering

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-
 import { connect } from "react-redux";
+
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden, cartItemCount }) => (
+const CartIcon = ({ toggleCartHidden, cartItemsCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{cartItemCount}</span>
+    <span className="item-count">{cartItemsCount}</span>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItemCount: cartItems.reduce(
-    (acc, cartItem) => acc + cartItem.quantity,
-    0
-  ),
+const mapStateToProps = (state) => ({
+  cartItemsCount: selectCartItemsCount(state),
 });
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
